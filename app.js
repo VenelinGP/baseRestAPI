34852f797e4f322a4b601b6531dd586b29f2670f
VenelinGP <venelin.petkov@netcoms.eu>
2021-06-22 00:47:34 +0300

    Updated REST API

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv/config');
 const cors = require('cors');
@@ -8,7 +7,8 @@ require('dotenv/config');
 
 // Middlewares
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 const postsRoute = require('./routes/posts');
 app.use('/posts', postsRoute);
